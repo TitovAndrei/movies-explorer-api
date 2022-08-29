cda16204a6f9ee6497507c3686adcae003486624
Andrey Titov <n3kto.1@yandex.ru>
2022-08-29 19:21:19 +0300

    hotfixed: исправил добавление лайка

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.patch(
   '/me',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().email(),
+      email: Joi.string().email(),
       name: Joi.string().min(2).max(30).required(),
     }),
   }),
